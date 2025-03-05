d3a31ef01e542d0b354733d8d0c6d8f85337ac04
Youmna Elgzery <130307273+youmnaabdelhamid1@users.noreply.github.com>
2025-03-05 20:54:22 +0200

    W4 Task1.1.dart

---
+++ Week 4/task1.1.dart
@@ -0,0 +1,22 @@
+//Please, prepare a presentation about the content and describe the 4 priciples of OOP (Abstraction, Polymorphism, Inheritance, Encapsulation)
+  Object-Oriented Programming (OOP)?
+A programming paradigm based on objects that interact with each other. It focuses on code reusability, modularity, and organization.
+    It makes code more structured 
+    طريقة بننظم بيها الكود مبنية على objects  كل object بيمثل حاجة فى الواقع وله خصائص او صفات بنسميها (attributes) و  (Methods )وظائف أو أفعال بنسميها  
+
+=> the Four Key Principles of OOP
+    1) Abstraction  means hiding complex implementation details and exposing only the necessary functionalities. 
+    بنخفى التفاصيل الداخلية للكود ونظهر فقط ما يحتاجه المستخدم 
+
+    2) Polymorphism
+ مبدأ تعدد الأشكال .. وظيفة واحدة لها أشكال مختلفة   
+  بمعنى إن نفس ال(function) أو ال(class)  
+    object ممكن يشتغل بطرق مختلفة حسب السياق .. بتختلف حسب استخدامها مع كل 
+
+    3) Inheritance الوراثة
+    بتسمح بإنشاء class جديد (child) 
+    بيكون وارث من class تانى موجود (parent) 
+    بيورث منه الصفات (attributes) و ال(functions ) الموجودة بدل من تكرار نفس الكود
+
+    4)Encapsulation 
+    معناها التغليف يعنى حماية البيانات أو إخفائها داخل ال class وبيمنع الوصول إليها مباشرة إلا من خلال دوال معينة private  class
